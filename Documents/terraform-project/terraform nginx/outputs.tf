77c9eda603420761e040e67a64ee8a0a5034e1e6
vyshnavipriya52-ux <vyshnavipriya52@gmail.com>
2026-05-22 20:08:17 +0530

    Delete Documents/terraform-project directory

--- Documents/terraform-project/terraform nginx/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "pubilc_ip" {
-    description = "Public IP of EC2"
-    value = "aws_instance.nginx_ec2.pubilc_ip
-}
--- Documents/terraform-project/terraform s3/terraform modules/modules/s3/main.tf
+++
@@ -1,8 +0,0 @@
-resource ""aws_s3_bucket" "mys3" {
-    bucket = "my-tf-test-bucket"
-
-   tags ={
-        Name        = "My bucket"
-        Environment = "Dev"
-   }
-}
